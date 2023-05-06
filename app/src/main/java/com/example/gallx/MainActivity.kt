a736619d4ae7667f21554e8cccb98330f6686722
Pratyush-Remon <pratyushsoni2004@gmail.com>
2023-05-06 13:14:27 +0530

    Feature Add : To list Images in Internal storage

--- app/src/main/java/com/example/gallx/MainActivity.kt
+++ app/src/main/java/com/example/gallx/MainActivity.kt
@@ -1,11 +1,105 @@
 package com.example.gallx
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.database.Cursor
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AbsListView
+import android.widget.BaseAdapter
+import android.widget.GridView
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
+    lateinit var rs: Cursor
+
+    private lateinit var gridView: GridView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        gridView = findViewById(R.id.gridview)
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                ),
+                121
+            )
+        } else {
+            listImages()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 121 && grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+            listImages()
+        }
+    }
+
+    private fun listImages() {
+        val cols = listOf(MediaStore.Images.Thumbnails.DATA).toTypedArray()
+        rs = contentResolver.query(
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            cols,
+            null,
+            null,
+            null
+        )!!
+        if (rs.moveToNext()){
+            Toast.makeText(applicationContext, rs.getString(0), Toast.LENGTH_LONG).show()
+            gridView.adapter = ImageAdapter(applicationContext)
+        }
+    }
+
+    inner class ImageAdapter : BaseAdapter {
+        lateinit var context : Context
+
+        constructor(context: Context){
+            this.context = context
+        }
+
+        override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
+            val iv = ImageView(context)
+            rs.moveToPosition(p0)
+            val path = rs.getString(0)
+            val bitmap = BitmapFactory.decodeFile(path)
+            iv.setImageBitmap(bitmap)
+            iv.layoutParams = AbsListView.LayoutParams(300, 300)
+            return iv
+        }
+
+        override fun getItem(p0: Int): Any {
+            return p0
+        }
+
+        override fun getItemId(p0: Int): Long {
+            return p0.toLong()
+        }
+
+        override fun getCount(): Int {
+            return rs.count
+        }
     }
 }
